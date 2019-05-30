d5bba237a355723c1c1ced8a0d4dfc84659c5cce
Vi Almeida <44755904+ViAlmeida44@users.noreply.github.com>
2019-05-30 13:45:36 -0300

    Update contracheque.cpp

--- contracheque.cpp
+++ contracheque.cpp
@@ -11,18 +11,18 @@ main(){
 	double salarioBase, inss, impostoRenda, fgts, valeTransporte , salarioFamilia, valorLiquido, vencimentos, descontos;
 	bool confirmaFilho, erro = true;
 
-	// Inserção de Dados
+	/* Inserção de Dados */
 	cout << "\nInsira seu nome: ";
 	cin >> nome;
 	
 	cout << "\nInsira o quanto você ganha: R$";;
 	cin >> salarioBase;
-	
+
+	/* Verificação se a pessoa tem filho ou não */	
 	while (erro == true) {
 		cout << "\nVocê por acaso tem filho(a) menor de 14 anos? Diga 1 para sim ou 0 não: ";
 		cin >> valorFilho;
 		
-		//Verificação se a pessoa tem filho ou não
 		if (valorFilho == 1) {
 			confirmaFilho = true;
 			erro = false;
@@ -36,7 +36,7 @@ main(){
 		}
 	}
 		
-	// Cálculo do INSS
+	/* Cálculo do INSS */
 	if (salarioBase <= 1751.81) {
 		inss = salarioBase * 0.08;
 	} else if (salarioBase > 1751.82 && salarioBase <= 2919.72) {
@@ -47,7 +47,7 @@ main(){
 		inss = 570.88;
 	}
 	
-	// Cálculo do Imposto de Renda
+	/* Cálculo do Imposto de Renda */
 	if (salarioBase <= 1903.98) {
 		impostoRenda = 0;
 	} else if (salarioBase > 1903.98 && salarioBase <= 2826.65) {
@@ -62,7 +62,7 @@ main(){
 		cout << "Insira um valor válido. (0 ou é 1) \n";
 	}
 	
-	// Cálculo do Salário Família
+	/* Cálculo do Salário Família */
 	if (confirmaFilho == true){
 		if (salarioBase <= 907.77) {
 			salarioFamilia = 46.54;
@@ -73,20 +73,20 @@ main(){
 		}
 	}
 	
-	// Cálculo FGTS
+	/* Cálculo do FGTS */
 	fgts = salarioBase * 0.08;
 	
-	// Vale Transporte
+	/* Cálculo do Vale Transporte */
 	valeTransporte = salarioBase * 0.06;
 	
-	// Soma dos vencimentos e descontos
+	/* Soma dos vencimentos e descontos */
 	vencimentos = salarioFamilia + valeTransporte;
 	descontos = inss + impostoRenda;
 	
-	// Cálculo do valor líquido
+	/* Cálculo do valor líquido */
 	valorLiquido = salarioBase + vencimentos - descontos;
 		
-	// Retornos
+	/* Retornos e Resultados dos Cálculos */
 	cout << "\n\n" << nome << ", você paga: \n\nR$" << inss << " de INSS; \n\n";
 	
 	if (salarioBase <= 1903.98) {
